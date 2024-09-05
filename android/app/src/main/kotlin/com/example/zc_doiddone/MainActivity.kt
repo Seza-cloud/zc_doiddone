dbe36ce7ed2546899ce5981d5a0e01786727df81
Seza-cloud <tfgelnov@gmail.com>
2024-09-05 20:02:00 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/zc_doiddone/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.zc_doiddone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:zc_doiddone/App/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
